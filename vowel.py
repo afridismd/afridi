000ab84bf57ca93841af2229be1d3ef60a124ce5
afridismd <41103360+afridismd@users.noreply.github.com>
2018-07-11 14:56:37 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+a=(raw_input())
+if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u" or a=="A" or a=="E" or a=="I" or a=="O" or a=="U"):
+	print("vowel")
+elif(a=="b" or a=="c" or a=="d" or a=="f" or a=="g" or a=="h" or a=="j" or a=="k" or a=="l" or a=="m" or a=="n" or a=="p" or a=="q" or a=="r" or a=="s" or a=="t" or a=="v" or a=="w" or a=="x" or a=="y" or a=="z" or a=="B" or a=="C" or a=="D" or a=="F" or a=="G" or a=="H" or a=="J" or a=="K" or a=="L" or a=="M" or a=="N" or a=="P" or a=="Q" or a=="R" or a=="S" or a=="T" or a=="V" or a=="W" or a=="X" or a=="Y" or a=="Z" ):
+	print("consonants")
+else:
+	print("invalid")
+ 
